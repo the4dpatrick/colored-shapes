3ca038b0ac9a8a728a323e8b88bdac9a7a67516a
Patrick <the4dpatrick@yahoo.com>
2013-08-07 01:23:37 +0900

    init commit

---
+++ script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	$('#tri, #random').click(function(){
+		var randomColor = function(){
+			return '#' + Math.random().toString(16).slice(2,8);
+		}
+		var rc1 = randomColor();
+		var rc2 = randomColor();
+		var rc3 = randomColor();
+		var rc4 = randomColor();
+		var rc5 = randomColor();
+		$('#tri').css("border-bottom", "100px solid " + rc1);
+		$('#square').css("background", rc2);
+		$('#pentagon').css("border-top-color", rc3);
+		$('#top-pent').css("border-bottom-color", rc3);
+		$('#circle').css('background', rc4);
+		$('.container').css('background', rc5);
+		$('#tri').html(rc1);
+		$('#square').html(rc2);
+		$('#pentagon').html("<p>" + rc3 + "</p>");
+		$('#circle').html("<p>" + rc4 + "</p>");
+		$('#bgcolor').html("<p>" + rc5 + "</p>");
+	})
+})
